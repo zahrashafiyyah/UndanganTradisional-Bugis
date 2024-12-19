ca495bf2d91fba21625664515d6a5a45223665d0
Zahra Shafiyyah Putri <zahrashafiyyah23@gmail.com>
2024-12-19 22:38:33 +0700

    add wedding

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,8 @@
-function bukaUndangan() {
-    const h1 = document.querySelector('#hero');
-    const n1 = document.querySelector('#navbar_wrapper');
-    const q1 = document.querySelector('#quotes');
-    const p1 = document.querySelector('#profil');
-    const a1 = document.querySelector('#acara');
-    const m1 = document.querySelector('#gallery');
-    const k1 = document.querySelector('#tamu');
-    const r1 = document.querySelector('#RSVP');
-    const s1 = document.querySelector('#story');
-    const g1 = document.querySelector('#gift');
-    const d1 = document.querySelector('#ucapan');
-    const t1 = document.querySelector('#thanks');
+const musik = document.querySelector('#kotak-musik');
+        musik.style.display = 'inline-block';
 
-const tombolBuka = document.querySelector('#tombolBukaUndangan'); 
-tombolBuka.addEventListener('click', function() {
-    console.log('Tombol ditekan!');
-    bukaUndangan();
-});
-
-    
-    h1.style.setProperty('display', 'none', 'important');
-    n1.style.setProperty('display', 'block', 'important');
-    q1.style.setProperty('display', 'block', 'important');
-    p1.style.setProperty('display', 'block', 'important');
-    a1.style.setProperty('display', 'block', 'important');
-    m1.style.setProperty('display', 'block', 'important');
-    k1.style.setProperty('display', 'block', 'important');
-    r1.style.setProperty('display', 'block', 'important');
-    s1.style.setProperty('display', 'block', 'important');
-    g1.style.setProperty('display', 'block', 'important');
-    d1.style.setProperty('display', 'block', 'important');
-    t1.style.setProperty('display', 'block', 'important');
-
-    
-
-   const musik = document.querySelector('#kotak-musik');
-        musik.style.display = 'block';
-
-    const lagu = document.querySelector('#lagu');
+const lagu = document.querySelector('#lagu');
         lagu.play();
-}
 
 function putarLagu (){
     const lagu = document.querySelector('#lagu');
